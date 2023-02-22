f0678c97776ca21460134945f29b6c85db920b6d
dimasambo <87640613+dimasambo@users.noreply.github.com>
2023-02-22 21:21:38 +0200

    Deploy proj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import tg from './images/telegram.png'
+import bg from './images/bg-tg6.png'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img className={'tg-logo'} src={tg} alt={'tg'}/>
+                <span className={'logo-name'}>Telegram</span>
+            </header>
+            <div className={'App-body'}>
+                <img src={bg} className={'body-bg'} alt={'bg'}/>
+                <div className={'info-block'}>
+                    <div className={'tg-name'}>
+                        TRADER | Алексей Мирнов
+                    </div>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Join
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
