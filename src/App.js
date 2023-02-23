c798891b0e7ed8c81415a57f74aaeca41fa7bfdb
dimasambo <87640613+dimasambo@users.noreply.github.com>
2023-02-23 16:02:20 +0200

    true telegram invite

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import tg from './images/telegram.png'
 import bg from './images/bg-tg6.png'
+import btc from './images/tg-btc-logo.jpg'
 
 function App() {
     return (
@@ -13,8 +13,20 @@ function App() {
             <div className={'App-body'}>
                 <img src={bg} className={'body-bg'} alt={'bg'}/>
                 <div className={'info-block'}>
+                    <div className={'tg-logo-circle'}>
+                        <img src={btc} alt={'btc-logo'}/>
+                    </div>
                     <div className={'tg-name'}>
                         TRADER | Алексей Мирнов
+                    </div>
+                    <div className={'tg-followers'}>
+                        3000+ subscribers
+                    </div>
+                    <div className={'tg-add-info'}>
+                        💎Место, которое ты долго искал!
+                    </div>
+                    <div className={'tg-work-with'}>
+                        ✔️Сотрудничество: <a href={'https://t.me/AleksProdu'} target="_blank">@AleksProdu</a>
                     </div>
                     <a
                         className="App-link"
@@ -22,8 +34,11 @@ function App() {
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        Join
+                        Join Channel
                     </a>
+                    <div className={'tg-invited'}>
+                        You are invited to the channel <span className={'bold'}>TRADER | Алексей Мирнов</span>. Click above to join.
+                    </div>
                 </div>
             </div>
         </div>
